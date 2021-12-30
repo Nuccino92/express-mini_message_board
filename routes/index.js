12a5874b7ec6958057b42c74f90573a0d2578575
Nuccino92 <anthnucci@hotmail.com>
2021-12-29 19:00:51 -0500

    added new view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Anthony",
+    added: new Date(),
+  },
+  { text: "Hello World!", user: "Charles", added: new Date() },
+];
+
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.post("/new", (req, res, next) => {
+  text = req.body.text;
+  user = req.body.user;
+  added = req.body.added;
+
+  messages.push({ text: text, user: user, added: added });
+  res.redirect("/");
 });
 
 module.exports = router;
